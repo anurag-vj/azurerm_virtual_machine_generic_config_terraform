24a207ad3a261899f59e0bd6850b474e8e0a6905
Anurag Vijay <anuxperia.c@gmail.com>
2024-11-30 20:01:49 +0530

    code modified

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "#Provide"
+      version = "4.12.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "#Provide"
+  subscription_id = "077db605-017b-40d2-82f1-5572039ac98d"
 }
